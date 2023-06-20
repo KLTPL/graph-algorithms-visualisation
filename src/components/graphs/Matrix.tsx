5213c0e797694f448fa79eeffc4679a701fd1ce4
Kacper Lebiedzinski <kltpl@duck.com>
2023-06-20 16:10:17 +0200

    Added a dot on current field to the visualisation

--- src/components/graphs/Matrix.tsx
+++ src/components/graphs/Matrix.tsx
@@ -69,12 +69,24 @@ function getColorClassNamesForField(visualisationData: VisualisationDataMatrix, 
   const isEmpty = (fieldType === FieldTypesMatrixGraph.empty);
   const isRock = (fieldType === FieldTypesMatrixGraph.rock);
   const isFieldVisited = isStepAlreadyMade(currStepIdx, listOfSteps, r, c);
+  const isCurrField = isFieldCurrFiled(visualisationData, currStepIdx, r, c);
   const isReachedEndNode = isStartEndNode && isFieldVisited;
   const isOnBacktrack = isNodeOnBacktrack(visualisationData, backtrackCount, r, c);
 
-  return (
-    `${(isEmpty && !isStartOrEnd && !isOnBacktrack) ? "bg-marixGraphFieldEmpty" : ""} ${(isStartOrEnd && !isReachedEndNode) ? "bg-startAndEndNode" : ""} ${(isRock) ? "bg-rock": ""} ${(isFieldVisited && !isReachedEndNode && !isOnBacktrack) ? "bg-primary" : ""} ${(isReachedEndNode) ? "bg-green" : ""} ${(isOnBacktrack) ? "bg-orange" : ""}`
-  );
+  const conditionAndValuePairs = [
+    [isEmpty && !isStartOrEnd && !isOnBacktrack, "bg-marixGraphFieldEmpty"],
+    [isStartOrEnd && !isReachedEndNode, "bg-startAndEndNode"],
+    [isRock, "bg-rock"],
+    [isFieldVisited && !isReachedEndNode && !isOnBacktrack, "bg-primary"],
+    [isCurrField, "after:content-[''] after:rounded-[50%] after:bg-black after:w-[7px] after:h-[7px]"],
+    [isReachedEndNode, "bg-green"],
+    [isOnBacktrack, "bg-orange"],
+  ];
+
+  return conditionAndValuePairs
+    .filter(arr => arr[0])
+    .map(arr => arr[1])
+    .join(" ");
 }
 
 function isStepAlreadyMade(currStepIdx: number, listOfSteps: FieldMatrixGraph[], r: number, c: number): boolean {
@@ -100,10 +112,19 @@ function isNodeOnBacktrack(visualisationData: VisualisationDataMatrix, backtrack
   return false;
 }
 
+function isFieldCurrFiled(visualisationData: VisualisationDataMatrix, currStepIdx: number, r: number, c: number): boolean {
+  const field = visualisationData.listOfSteps[currStepIdx];
+  return (
+    field !== undefined &&
+    field.x === c &&
+    field.y === r
+  );
+}
+
 function Field({ className }: FieldProps) {
   return (
     <div 
-      className={`aspect-square ${className}`}
+      className={`aspect-square grid place-content-center ${className}`}
     />
   );
 }

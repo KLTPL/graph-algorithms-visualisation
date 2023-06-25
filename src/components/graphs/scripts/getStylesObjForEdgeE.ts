f4ed2bd6b43cf4dc7f8f93503c9f24772f373af9
Kacper Lebiedzinski <kltpl@duck.com>
2023-06-25 21:14:39 +0200

    Added a mechanism for showing edge cost, but not showing proper cost yet

--- src/components/graphs/scripts/getStylesObjForEdgeE.ts
+++ src/components/graphs/scripts/getStylesObjForEdgeE.ts
@@ -1,21 +1,13 @@
+import { EdgeData } from "../elements/EdgeU";
 import { NODE_SIZE_PX } from "../elements/NodeE";
-import { NodePosition } from "./getProperNodesPostions";
 
 const EDGE_HEIGHT_PX = 4;
 
-function getBasicStylesForEdgeE(
-  nodePos1: NodePosition,
-  nodePos2: NodePosition
-) {
-  const distX = nodePos1.left - nodePos2.left;
-  const distY = nodePos1.top - nodePos2.top;
-  const angle = Math.atan(distY / distX) * (180 / Math.PI);
-  const dist = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
-  const { left, top } = nodePos1.left < nodePos2.left ? nodePos1 : nodePos2;
+function getBasicStylesForEdgeE({ angle, width, left, top }: EdgeData) {
   return {
     transformOrigin: "left",
     height: `${EDGE_HEIGHT_PX}px`,
-    width: `${dist}%`,
+    width: `${width}%`,
     left: `${left}%`,
     top: `${top}%`,
     transform: `translate(${NODE_SIZE_PX / 2}px, ${

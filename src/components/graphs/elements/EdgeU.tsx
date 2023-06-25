6e4bd798c5ffdddb767087215ba01d6ce955488a
Kacper Lebiedziński <k.lebiedzinski04.gmail.com>
2023-06-25 21:14:39 +0200

    Added a mechanism for showing edge cost, but not showing proper cost yet

--- src/components/graphs/elements/EdgeU.tsx
+++ src/components/graphs/elements/EdgeU.tsx
@@ -7,6 +7,7 @@ import getBasicStylesForEdgeE from "../scripts/getStylesObjForEdgeE";
 import { getClassNamesForEdgeU } from "../scripts/getClassNamesForEdgeE";
 import { NodePosition } from "../scripts/getProperNodesPostions";
 import { Edge } from "../scripts/getEdges";
+import { UserGraphTypes } from "../../../visualisationData/typesGraphData";
 
 interface EdgeUndirectedProps {
   nodePos1: NodePosition;
@@ -25,7 +26,11 @@ function EdgeUndirected({
     .visualisationData as VisualisationDataDW;
   const { currStepIdx } = useUserInputData();
 
-  const stylesObj = getBasicStylesForEdgeE(nodePos1, nodePos2);
+  const isWeighted =
+    visualisationData.graphType === UserGraphTypes.DW ||
+    visualisationData.graphType === UserGraphTypes.UW;
+  const edgeData = calcEdgeData(nodePos1, nodePos2);
+  const stylesObj = getBasicStylesForEdgeE(edgeData);
   const classNames = getClassNamesForEdgeU(
     edge,
     visualisationData,
@@ -33,7 +38,44 @@ function EdgeUndirected({
     backtrackCount
   );
 
-  return <div style={stylesObj} className={classNames} />;
+  return (
+    <div style={stylesObj} className={classNames}>
+      {isWeighted && (
+        <input
+          type="number"
+          defaultValue={1}
+          className="absolute left-1/2 top-1/2 max-w-[4ch] bg-marixGraphFieldEmpty text-center border-2 border-nodeDefault rounded-full"
+          style={{
+            transform: `translate(-50%, -50%) rotate(${-edgeData.angle}deg)`,
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export interface EdgeData {
+  angle: number;
+  width: number;
+  left: number;
+  top: number;
+}
+
+function calcEdgeData(
+  nodePos1: NodePosition,
+  nodePos2: NodePosition
+): EdgeData {
+  const distX = nodePos1.left - nodePos2.left;
+  const distY = nodePos1.top - nodePos2.top;
+  const angle = Math.atan(distY / distX) * (180 / Math.PI);
+  const width = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
+  const { left, top } = nodePos1.left < nodePos2.left ? nodePos1 : nodePos2;
+  return {
+    angle,
+    width,
+    left,
+    top,
+  };
 }
 
 export default EdgeUndirected;

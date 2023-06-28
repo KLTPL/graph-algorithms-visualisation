40732e0cd1b2eaaedd1a17538ae1b1b96ef1c492
Kacper Lebiedzinski <kltpl@duck.com>
2023-06-28 15:58:40 +0200

    Finished the pointer tools for M

--- src/components/visualisation-container/nodes/NodeM.tsx
+++ src/components/visualisation-container/nodes/NodeM.tsx
@@ -1,5 +1,11 @@
-import { useUserInput, useVisualisationData } from "../../../context/Context";
+import {
+  useUserInput,
+  useVisualisationData,
+  useVisualisationPointerTools,
+} from "../../../context/Context";
+import { NodeTypesM } from "../../../visualisationData/typesGraphData";
 import { VisualisationDataM } from "../../../visualisationData/typesVisualisationData";
+import { VisualisationPointerToolsM } from "../../visualisation-tools/VisualisationTools";
 import { getClassNamesForNodeM } from "../scripts/getClassNamesForNodeE";
 
 type NodeMatrixProps = {
@@ -9,8 +15,12 @@ type NodeMatrixProps = {
 };
 
 function NodeMatrix({ backtrackCount, r, c }: NodeMatrixProps) {
-  const visualisationData = useVisualisationData()
-    .visualisationData as VisualisationDataM;
+  const { visualisationData: AnyVisualisationData, refreshVisualisationData } =
+    useVisualisationData();
+  const visualisationData = AnyVisualisationData as VisualisationDataM;
+  console.log(visualisationData.startNode);
+  const pointerTool = useVisualisationPointerTools()
+    .pointerTool as VisualisationPointerToolsM;
   const { currStepIdx } = useUserInput();
   const className = getClassNamesForNodeM({
     visualisationData,
@@ -19,7 +29,29 @@ function NodeMatrix({ backtrackCount, r, c }: NodeMatrixProps) {
     r,
     c,
   });
-  return <div className={className} />;
+  function handleOnPointerDown(): void {
+    switch (pointerTool) {
+      case VisualisationPointerToolsM.EmptyField:
+        visualisationData.graph[r][c] = NodeTypesM.empty;
+        break;
+      case VisualisationPointerToolsM.RockField:
+        visualisationData.graph[r][c] = NodeTypesM.rock;
+        break;
+      case VisualisationPointerToolsM.StartField:
+        visualisationData.startNode.x = c;
+        visualisationData.startNode.y = r;
+        break;
+      case VisualisationPointerToolsM.EndField:
+        visualisationData.endNode.x = c;
+        visualisationData.endNode.y = r;
+        break;
+    }
+    if (pointerTool !== VisualisationPointerToolsM.NoTool) {
+      refreshVisualisationData();
+    }
+  }
+
+  return <div className={className} onPointerDown={handleOnPointerDown} />;
 }
 
 export default NodeMatrix;

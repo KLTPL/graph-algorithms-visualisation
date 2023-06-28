8010775ae8cc01fa31b7d47f39ffaecb9338974d
Kacper Lebiedzinski <kltpl@duck.com>
2023-06-28 20:25:28 +0200

    Finished the remove node system

---
+++ src/components/visualisation-container/scripts/removeNode.ts
@@ -0,0 +1,45 @@
+import { NodeE } from "../../../visualisationData/typesGraphData";
+import { VisualisationDataDW } from "../../../visualisationData/typesVisualisationData";
+import getProperNodesPosition from "./getProperNodesPostions";
+
+export default function removeNode(
+  visualisationData: VisualisationDataDW,
+  node: NodeE,
+  refreshVisualisationData: () => void
+) {
+  visualisationData.graph.splice(node, 1);
+  getProperNodesPosition(visualisationData).splice(node, 1);
+  changeStartAndEndNodeIfNeeded(visualisationData, node);
+  for (const neighbours of visualisationData.graph) {
+    for (let i = 0; i < neighbours.length; i++) {
+      if (neighbours[i].node === node) {
+        neighbours.splice(i, 1);
+        i--;
+        continue;
+      }
+      if (neighbours[i].node > node) {
+        neighbours[i].node--;
+      }
+    }
+  }
+  refreshVisualisationData();
+}
+
+function changeStartAndEndNodeIfNeeded(
+  { startNode, endNode }: VisualisationDataDW,
+  nodeToRemove: NodeE
+) {
+  if (startNode.current >= nodeToRemove && startNode.current > 0) {
+    startNode.current--;
+  }
+  if (endNode.current >= nodeToRemove && endNode.current > 0) {
+    endNode.current--;
+  }
+  if (startNode.current === endNode.current) {
+    if (startNode.current === 0) {
+      startNode.current++;
+    } else {
+      startNode.current--;
+    }
+  }
+}

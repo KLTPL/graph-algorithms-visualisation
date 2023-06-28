c6ad1f77b0a5dc482c88cc6546d56f3efe7fee8c
Kacper Lebiedziński <k.lebiedzinski04.gmail.com>
2023-06-28 17:28:48 +0200

    Added highlighting to the selection of pointer tools (to show current tool)

--- src/components/visualisation-tools/ToolRadioInput.tsx
+++ src/components/visualisation-tools/ToolRadioInput.tsx
@@ -7,19 +7,20 @@ import { NODE_SIZE_PX } from "../visualisation-container/nodes/NodeE";
 import { VisualisationPointerTools as Tools } from "./VisualisationTools";
 
 const DEFAULT_CLASS_NAME_M =
-  "w-[50px] aspect-square border-solid border-2 border-nodeBorder text-black rounded-xl grid place-content-center font-semibold";
+  "w-[50px] aspect-square border-solid border-4 border-nodeBorder text-black rounded-xl grid place-content-center font-semibold";
 const DEFAULT_CLASS_NAME_E =
-  "aspect-square border-solid border-2 border-nodeBorder text-black rounded-full font-semibold flex items-center justify-center relative";
+  "aspect-square border-solid border-4 border-nodeBorder text-black rounded-full font-semibold flex items-center justify-center relative";
+const CURRENT_CLASS_NAME = "border-primary";
 
 interface ToolBoxProps {
-  className: string;
+  customClassName: string;
   title: string;
   newPointerTool: Tools;
   content?: string | JSX.Element | JSX.Element[];
 }
 
 export default function ToolRadioInput({
-  className,
+  customClassName,
   title,
   newPointerTool,
   content,
@@ -31,6 +32,10 @@ export default function ToolRadioInput({
     visualisationData.graphType === UserGraphTypes.M
       ? DEFAULT_CLASS_NAME_M
       : DEFAULT_CLASS_NAME_E;
+  const classNames = [defaultClassName, customClassName];
+  if (pointerTool === newPointerTool) {
+    classNames.push(CURRENT_CLASS_NAME);
+  }
   function handleOnClick() {
     if (newPointerTool === pointerTool) {
       setPointerToolToDefault();
@@ -41,7 +46,7 @@ export default function ToolRadioInput({
   return (
     <button
       style={{ width: NODE_SIZE_PX }}
-      className={[defaultClassName, className].join(" ")}
+      className={classNames.join(" ")}
       title={title}
       onClick={handleOnClick}
     >

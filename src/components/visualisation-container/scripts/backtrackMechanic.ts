9bf67c9b58ce3705748aeff7eff4774b74e37e9b
Kacper Lebiedzinski <kltpl@duck.com>
2023-06-29 10:52:50 +0200

    Cut the time of each backtrack step

--- src/components/visualisation-container/scripts/backtrackMechanic.ts
+++ src/components/visualisation-container/scripts/backtrackMechanic.ts
@@ -13,7 +13,7 @@ export function backtrackToFirstNode(
     do {
       count++;
       setBacktrackCount(count);
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 150));
     } while (isBacktracking.current && count <= pathLen);
   });
 }
